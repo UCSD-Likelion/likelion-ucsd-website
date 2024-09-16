a19dd8167f5a1f1fa12b489374979c7fb61dad7c
7174Andy <hep003@ucsd.edu>
2024-09-16 12:37:38 -0700

    add activated menu to navbar

---
+++ client/app/(home)/lessons/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+export default function Lessons() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Typography variant="h1">Lessons</Typography>
+    </Box>
+  );
+}
--- client/components/navigations.jsx
+++ client/components/navigations.jsx
@@ -5,11 +5,34 @@ import { Box, Typography, Button, styled } from "@mui/material";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-
+import { usePathname } from "next/navigation";
 import LikeLionLogo from "../assets/images/likelion-logo.png";
+
+const MenuBox = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  gap: "0.5rem",
+  paddingLeft: "1rem",
+  paddingRight: "1rem",
+  paddingTop: "0.25rem",
+  paddingBottom: "0.25rem",
+  borderRadius: "0.5rem",
+  cursor: "pointer",
+  ":hover": {
+    color: "white",
+    backgroundColor: "#1976d2",
+    transition: "all 0.3s ease-in-out",
+  },
+  "&.active": {
+    color: "blue",
+    fontWeight: "bold",
+  },
+});
 
 export default function Navigations() {
   const router = useRouter();
+  const pathname = usePathname();
+  console.log(pathname);
   return (
     <Box
       sx={{
@@ -40,16 +63,22 @@ export default function Navigations() {
           href="/lessons"
           style={{ textDecoration: "none", color: "black" }}
         >
-          <Typography variant="h6">Lessons</Typography>
+          <MenuBox className={pathname === "/lessons" ? "active" : ""}>
+            <Typography variant="h6">Lessons</Typography>
+          </MenuBox>
         </Link>
         <Link href="/about" style={{ textDecoration: "none", color: "black" }}>
-          <Typography variant="h6">About</Typography>
+          <MenuBox className={pathname === "/about" ? "active" : ""}>
+            <Typography variant="h6">About</Typography>
+          </MenuBox>
         </Link>
         <Link
           href="/projects"
           style={{ textDecoration: "none", color: "black" }}
         >
-          <Typography variant="h6">Projects</Typography>
+          <MenuBox className={pathname === "/projects" ? "active" : ""}>
+            <Typography variant="h6">Projects</Typography>
+          </MenuBox>
         </Link>
         <Button
           variant="contained"
