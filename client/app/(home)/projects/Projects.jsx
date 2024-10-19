460962b704051b05abe5aeb217661e0082628375
Kyla Dawon Park <correct-parkdawon5@gmail.com>
2024-10-18 22:09:11 -0700

    new design

--- client/app/(home)/projects/Projects.jsx
+++ client/app/(home)/projects/Projects.jsx
@@ -1,11 +1,12 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { Box, Typography, Stack, Grid, Card, CardMedia, CardContent } from "@mui/material";
-import Link from 'next/link';
+import { Box, Typography, Grid, Card, CardMedia, CardContent, IconButton} from "@mui/material";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm"; // Optional GitHub flavored markdown
 import rehypeRaw from "rehype-raw"; // Optional if you want to allow raw HTML
 import Image from "next/image";
+import styles from './Projects.module.css'; 
+import CloseIcon from '@mui/icons-material/Close';
 
 // Projects data
 const projectBoxes = [
@@ -35,6 +36,7 @@ const projectBoxes = [
 export default function Projects({ projects }) {
   const [selectedContent, setSelectedContent] = useState("");
   const [activeProject, setActiveProject] = useState(null);
+  const [showDetail, setShowDetail] = useState(false);
 
   const fetchMarkdownContent = async (linkTo) => {
     const response = await fetch(`blog2/${linkTo}`); // Adjust the path based on your setup
@@ -49,6 +51,13 @@ export default function Projects({ projects }) {
   const handleItemClick = (project) => {
     fetchMarkdownContent(project.linkTo);
     setActiveProject(project.title);
+    setShowDetail(true);
+  };
+
+  const handleClose = () => {
+    setShowDetail(false);
+    setSelectedContent("");
+    setActiveProject(null);
   };
 
   useEffect(() => {
@@ -65,6 +74,7 @@ export default function Projects({ projects }) {
         flexGrow: 1,
         padding: 4,
         marginLeft: "-270px",
+        marginRight: "-500px",
         marginTop: "80px",
         padding: "1rem",
       }}
@@ -76,13 +86,21 @@ export default function Projects({ projects }) {
           paddingBottom: "180px",
         }}
       >
-        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", px: "1rem", py: "1rem" }}>
-          <Typography variant="h1" sx={{ marginTop: 12, marginBottom: 8 }}>Projects</Typography>
-          <Grid container spacing={2} justifyContent="center">
-            {projectBoxes.map((project) => (
-              <Grid item xs={12} sm={6} md={4} key={project.id}>
-                <div onClick={() => handleItemClick(project)}>
-                  <Card sx={{ cursor: 'pointer' }}>
+        <Box sx={{ flexGrow: 1, padding: 4, marginLeft: "-270px", marginTop: "80px", padding: "1rem" }}>
+          <Box sx={{ maxWidth: "1000px", margin: "0 auto", paddingBottom: "180px" }}>
+            <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", px: "1rem", py: "1rem" }}>
+              <Typography variant="h1" sx={{ marginTop: 12, marginBottom: 8 }}>Projects</Typography>
+              {projectBoxes.map((project) => (
+                <div 
+                  key={project.id} 
+                  className={styles.bubble} 
+                  // style={{
+                  //   top: `${Math.random() * 400}px`, 
+                  //   left: `${Math.random() * 800}px` 
+                  // }} 
+                  onClick={() => handleItemClick(project)}
+                >
+                  <Card className={styles.card}>
                     <CardMedia component="img" height="140" image={project.imageUrl} alt={project.title} />
                     <CardContent>
                       <Typography variant="h5">{project.title}</Typography>
@@ -90,144 +108,150 @@ export default function Projects({ projects }) {
                     </CardContent>
                   </Card>
                 </div>
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
-        <Box sx={{ marginTop: 6 }}>
-          <ReactMarkdown
-            children={selectedContent}
-            remarkPlugins={[remarkGfm]} // Optional GitHub flavor
-            rehypePlugins={[rehypeRaw]} // Optional if you're using HTML in markdown
-            components={{
-              h1: ({ node, ...props }) => (
-                <Typography
-                  variant="h3"
-                  gutterBottom
-                  sx={{
-                    fontFamily: "Noto Sans, sans-serif",
-                    color: "#162548",
-                  }}
-                  {...props}
-                />
-              ),
-              h2: ({ node, ...props }) => (
-                <Typography
-                  variant="h4"
-                  gutterBottom
-                  sx={{
-                    fontFamily: "Noto Sans, sans-serif",
-                    marginTop: "2.5rem",
-                    marginBottom: "1rem",
-                    fontWeight: "bold",
-                    fontSize: "32px",
-                  }}
-                  {...props}
-                />
-              ),
-              h3: ({ node, ...props }) => (
-                <Typography
-                  variant="h5"
-                  gutterBottom
-                  sx={{
-                    fontFamily: "Noto Sans, sans-serif",
-                    marginTop: "2.2rem",
-                    marginBottom: "1rem",
-                    fontWeight: "bold",
-                    fontSize: "24px",
-                  }} // Apply Noto Sans
-                  {...props}
-                />
-              ),
-              p: ({ node, ...props }) => {
-                if (node.children[0]?.tagName === "img") {
-                  const image = node.children[0];
-                  return (
-                    <Box
-                      className="image"
-                      sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                        marginBottom: "1rem",
-                        width: "100%",
-                      }}
-                    >
-                      <Image
-                        src={`/images${image.properties.src}`}
-                        alt={image.properties.alt}
-                        width={1000}
-                        height={600}
-                        style={{
-                          borderRadius: "8px",
-                          height: "auto",
-                          width: "80%",
+              ))}
+            </Box>
+            {showDetail && (
+              <Box className={styles.detailCard}>
+                <IconButton onClick={handleClose} sx={{ position: 'absolute', top: 16, right: 16 }}>
+                  <CloseIcon />
+                </IconButton>
+                <Typography variant="h4">{activeProject}</Typography>
+                <ReactMarkdown
+                  children={selectedContent}
+                  remarkPlugins={[remarkGfm]} // Optional GitHub flavor
+                  rehypePlugins={[rehypeRaw]} // Optional if you're using HTML in markdown
+                  components={{
+                    h1: ({ node, ...props }) => (
+                      <Typography
+                        variant="h3"
+                        gutterBottom
+                        sx={{
+                          fontFamily: "Noto Sans, sans-serif",
+                          color: "#162548",
                         }}
+                        {...props}
                       />
-                    </Box>
-                  );
-                }
-                return (
-                  <Typography
-                    variant="body1"
-                    paragraph
-                    sx={{
-                      fontFamily: "Noto Sans, sans-serif",
-                      marginBottom: "0.7rem",
-                      fontSize: "18px",
-                    }}
-                    {...props}
-                  />
-                );
-              },
-              a: ({ node, ...props }) => (
-                <Typography
-                  variant="body1"
-                  component="a"
-                  href={props.href}
-                  target="_blank" // Open in new tab
-                  rel="noopener noreferrer" // Security measure
-                  sx={{
-                    color: "#3a62ac",
-                    textDecoration: "underline",
-                    fontFamily: "Noto Sans, sans-serif",
-                    fontSize: "18px",
+                    ),
+                    h2: ({ node, ...props }) => (
+                      <Typography
+                        variant="h4"
+                        gutterBottom
+                        sx={{
+                          fontFamily: "Noto Sans, sans-serif",
+                          marginTop: "2.5rem",
+                          marginBottom: "1rem",
+                          fontWeight: "bold",
+                          fontSize: "32px",
+                        }}
+                        {...props}
+                      />
+                    ),
+                    h3: ({ node, ...props }) => (
+                      <Typography
+                        variant="h5"
+                        gutterBottom
+                        sx={{
+                          fontFamily: "Noto Sans, sans-serif",
+                          marginTop: "2.2rem",
+                          marginBottom: "1rem",
+                          fontWeight: "bold",
+                          fontSize: "24px",
+                        }} // Apply Noto Sans
+                        {...props}
+                      />
+                    ),
+                    p: ({ node, ...props }) => {
+                      if (node.children[0]?.tagName === "img") {
+                        const image = node.children[0];
+                        return (
+                          <Box
+                            className="image"
+                            sx={{
+                              display: "flex",
+                              flexDirection: "column",
+                              alignItems: "center",
+                              marginBottom: "1rem",
+                              width: "100%",
+                            }}
+                          >
+                            <Image
+                              src={`/images${image.properties.src}`}
+                              alt={image.properties.alt}
+                              width={1000}
+                              height={600}
+                              style={{
+                                borderRadius: "8px",
+                                height: "auto",
+                                width: "80%",
+                              }}
+                            />
+                          </Box>
+                        );
+                      }
+                      return (
+                        <Typography
+                          variant="body1"
+                          paragraph
+                          sx={{
+                            fontFamily: "Noto Sans, sans-serif",
+                            marginBottom: "0.7rem",
+                            fontSize: "18px",
+                          }}
+                          {...props}
+                        />
+                      );
+                    },
+                    a: ({ node, ...props }) => (
+                      <Typography
+                        variant="body1"
+                        component="a"
+                        href={props.href}
+                        target="_blank" // Open in new tab
+                        rel="noopener noreferrer" // Security measure
+                        sx={{
+                          color: "#3a62ac",
+                          textDecoration: "underline",
+                          fontFamily: "Noto Sans, sans-serif",
+                          fontSize: "18px",
+                        }}
+                        {...props}
+                      />
+                    ),
+                    li: ({ node, ...props }) => (
+                      <li
+                        style={{
+                          marginBottom: "0.4rem",
+                          fontFamily: "Noto Sans, sans-serif",
+                          marginLeft: "-15px",
+                          lineHeight: "1.5",
+                          fontSize: "18px",
+                        }}
+                      >
+                        {props.children}
+                      </li>
+                    ),
+                    hr: () => (
+                      <hr style={{ marginTop: "50px", marginBottom: "15px" }} />
+                    ),
+                    pre: ({ node, ...props }) => (
+                      <pre
+                        style={{
+                          backgroundColor: "#282c34",
+                          padding: "10px",
+                          borderRadius: "8px",
+                          color: "#fff",
+                          overflow: "auto",
+                          fontSize: "15px",
+                          fontFamily: "Noto Sans, sans-serif",
+                        }}
+                        {...props}
+                      />
+                    ),
                   }}
-                  {...props}
                 />
-              ),
-              li: ({ node, ...props }) => (
-                <li
-                  style={{
-                    marginBottom: "0.4rem",
-                    fontFamily: "Noto Sans, sans-serif",
-                    marginLeft: "-15px",
-                    lineHeight: "1.5",
-                    fontSize: "18px",
-                  }}
-                >
-                  {props.children}
-                </li>
-              ),
-              hr: () => (
-                <hr style={{ marginTop: "50px", marginBottom: "15px" }} />
-              ),
-              pre: ({ node, ...props }) => (
-                <pre
-                  style={{
-                    backgroundColor: "#282c34",
-                    padding: "10px",
-                    borderRadius: "8px",
-                    color: "#fff",
-                    overflow: "auto",
-                    fontSize: "15px",
-                    fontFamily: "Noto Sans, sans-serif",
-                  }}
-                  {...props}
-                />
-              ),
-            }}
-          />
+              </Box>
+            )}
+          </Box>
         </Box>
       </Box>
     </Box>
