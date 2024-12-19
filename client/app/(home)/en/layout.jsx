ce336ff7146ec1fbb05cbfcaec9825b4de366a40
7174Andy <hep003@ucsd.edu>
2024-12-18 20:10:03 -0800

    change metadata for en

--- client/app/(home)/en/layout.jsx
+++ client/app/(home)/en/layout.jsx
@@ -4,14 +4,21 @@ import { Box, Divider, ThemeProvider } from "@mui/material";
 import { theme } from "../../../theme";
 
 export const metadata = {
-  title: "Likelion UCSD | UCSD 멋쟁이사자처럼",
+  title: "Likelion UCSD | Home",
   description: "Likelion UCSD",
 };
 
 export default function RootLayout({ children }) {
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={{ display: "flex", flexDirection: "column", width: "100%", backgroundColor: "white" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+          backgroundColor: "white",
+        }}
+      >
         <Navigations />
         <Divider />
         {children}
