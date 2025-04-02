be663e76502355ac1a0e4562ab39a58a577b3f48
7174Andy <hep003@ucsd.edu>
2025-04-02 12:52:17 -0700

    add api for projects

--- client/app/api/projects/route.js
+++ client/app/api/projects/route.js
@@ -4,7 +4,7 @@ import Project from "../../../models/Projects";
 export const GET = async (req, res) => {
   try {
     await dbConnect();
-    const projects = await Projects.find();
+    const projects = await Project.find();
     return new Response(JSON.stringify(projects), {
       status: 200,
       headers: { "Content-Type": "application/json" },
@@ -44,6 +44,87 @@ export const POST = async (req, res) => {
       JSON.stringify({ message: "Project created successfully", newProject }),
       {
         status: 201,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ message: "❌ Connection failed", error: error.message }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+};
+
+export const DELETE = async (req, res) => {
+  const { id } = await req.json();
+
+  if (!id) {
+    return new Response(JSON.stringify({ message: "Please provide an ID" }), {
+      status: 400,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  try {
+    await dbConnect();
+    const deletedProject = await Project.findByIdAndDelete(id);
+    if (!deletedProject) {
+      return new Response(JSON.stringify({ message: "Project not found" }), {
+        status: 404,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+    return new Response(
+      JSON.stringify({ message: "Project deleted successfully" }),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ message: "❌ Connection failed", error: error.message }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+};
+
+export const PUT = async (req, res) => {
+  const { id, name, description, content } = await req.json();
+
+  if (!id || !name || !description || !content) {
+    return new Response(JSON.stringify({ message: "Please fill all fields" }), {
+      status: 400,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  try {
+    await dbConnect();
+    const updatedProject = await Project.findByIdAndUpdate(
+      id,
+      { name, description, content },
+      { new: true }
+    );
+    if (!updatedProject) {
+      return new Response(JSON.stringify({ message: "Project not found" }), {
+        status: 404,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+    return new Response(
+      JSON.stringify({
+        message: "Project updated successfully",
+        updatedProject,
+      }),
+      {
+        status: 200,
         headers: { "Content-Type": "application/json" },
       }
     );
